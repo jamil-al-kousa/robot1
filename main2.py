e393a6d5533a344e1bd73170839796d6ded1744a
Ahsa21 <129762410+Ahsa21@users.noreply.github.com>
2023-04-28 11:00:16 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -20,23 +20,13 @@ Claw_motor = Motor(Port.A)
 color_sensor = ColorSensor(Port.S2)
 base_switch = TouchSensor(Port.S1) # inte säker om det är s1, fråga eller checka!
 
+arm_motor.reset_ange(0)
+arm_motor.run_target(150, 35) # upp till it gets recognized of the color sensor.
 
 
+rgb = color_sensor.rgb()
 
+def rgb_function():
+    if (int(rgb[1] / int(rgb[0]))) < 0.5 and (int(rgb[2] / int(rgb[0]))) < 0.5:
+        print("Color is red")
 
-arm_motor.run_time(-30, 1000) # go ner för en secund med hastigheten -30 vikel
-Claw_motor
-arm_motor.run(15) # go upp med hastigheten 15 vikel upp per secund
-
-
-
-time1 = -30
-turning_motor.run(time1)
-while true:
-    if base_switch.pressed():
-        wait(1000)
-        turning_motor.reset_angel(0) # Jag är inte säker om det här steget behövs, testa det!
-        time1 = -time1
-        turning_motor.run(time1)
-
-    elif turning_motor.angle() == 0:
