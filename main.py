963f44d950217f536acc992afd2d55d8b81a09f7
jamil-al-kousa <108755186+jamil-al-kousa@users.noreply.github.com>
2023-05-02 12:29:59 +0200

    Add files via upload
    robot color sensor improved to pass the test cases of the 4 colors
    robot item placing is tested
    check if there is an item is also fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
@@ -13,77 +14,211 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
 ev3 = EV3Brick()
-turning_motor = Motor(Port.C)
+
+# Write your program here.
+ev3.speaker.beep()
+
+# Initialize a motor at port A.
+claw_motor= Motor(Port.A)
+
 arm_motor = Motor(Port.B)
-Claw_motor = Motor(Port.A)
+turning_motor = Motor(Port.C)
 color_sensor = ColorSensor(Port.S2)
-base_switch = TouchSensor(Port.S1)
+touch_sensor = TouchSensor(Port.S1)
 
 
 
-def rgb_function():
-    rgb = color_sensor.rgb()
-    begin = False
-    Done = False
-    # (rgb[0] < 85) and
-    print(rgb)
+def reboot():
+    
+    
+    while touch_sensor.pressed()== False :
+        turning_motor.run_angle(speed=300,rotation_angle=20, then=Stop.HOLD)
+    
 
-    if rgb[0] == 0 or rgb[1] == 0 or rgb[2] == 0:
-        try:
-                
-            print("move the box, pls!")
-            begin = True
-            wait(8000)
+    wait(1000)
 
-            rgb_function()
-        except:
-            print(".--..")
-        #break()
-
-    if begin == False and (((((int(rgb[2])) / int(rgb[1]))) <= 0.6) or (int(rgb[2]) / int(rgb[0])) <= 0.6): # check if it is yellow
-        if int(rgb[0]) < int(rgb[1]): 
-            if (int(rgb[2] / int(rgb[1]))) <= 0.6 and ((int(rgb[0])) / int(rgb[1]) >=0.8):
-                if (int(rgb[2] / int(rgb[0]))) <= 0.8:
-                    print("Color is yellow")
-                    Done = True
-
-            elif (int(rgb[2] / int(rgb[0]))) <= 0.6 and ((int(rgb[0])) / int(rgb[1]) >=0.8):
-                if (int(rgb[2] / int(rgb[1]))) <= 0.8:
-                    print("Color is yellow")
-                    Done = True
-        elif int(rgb[0]) > int(rgb[1]):
-            if (int(rgb[2] / int(rgb[1]))) <= 0.6 and ((int(rgb[1])) / int(rgb[0]) >=0.52):
-                if (int(rgb[2] / int(rgb[0]))) <= 0.8:
-                    print("Color is yellow")
-                    Done = True
-
-            elif (int(rgb[2] / int(rgb[0]))) <= 0.6 and ((int(rgb[1])) / int(rgb[0]) >=0.52):
-                if (int(rgb[2] / int(rgb[1]))) <= 0.8:
-                    print("Color is yellow")
-                    Done = True
-        else: # do it again in case it did not get any color
-            try:
-                rgb_function()
-            except: 
-                print("...")
-
-    if Done == False and begin == False: # If it is not read so check the rest of the colors
-        if (int(rgb[1] / int(rgb[0]))) < 0.5 and (int(rgb[2] / int(rgb[0]))) < 0.5:
-            print("Color is red")
-
-        elif (int(rgb[0] / int(rgb[1]))) < 0.5 and (int(rgb[2] / int(rgb[1]))) < 0.5:
-            print("Color is green")
-
-        elif (int(rgb[0] / int(rgb[2]))) < 0.5 and (int(rgb[1] / int(rgb[2]))) < 0.5:
-                print("Color is blue")
-        else: # if it got no color so do it again
-            try:
-                print("Movie the box, pls")
-                wait(5000)
-                rgb_function()
-                print("f")
-            except:
-                print("...")
+    claw_motor.run_until_stalled(215, then=Stop.HOLD, duty_limit=50)
+    arm_motor.run_until_stalled(150, then=Stop.COAST, duty_limit=10)
+    wait(2000)
+    turning_motor.reset_angle(0)
+    arm_motor.reset_angle(0)
+    claw_motor.reset_angle(0)
+    print(arm_motor.angle())
+    return
 
 
-rgb_function()
+#arm_motor.run_angle(speed=100,rotation_angle=-200, then=Stop.HOLD)
+
+#the robot can turn max 600 and 300 is the medium when reboot function is used
+#the max value that is good for using the arm is 400
+# claw motor open and close fully if it is -80 degree
+#If turning motor angle is plus the motor turn to left
+#if arm angle is minus it goes up
+
+
+def takethebox(position):
+    
+    #nextedtion 
+    # next round in the loop it would be down with open claw and att a position of the item
+    arm_motor.run_angle(speed=200,rotation_angle=-400)
+    print(arm_motor.angle())
+    claw_motor.run_until_stalled(50, then=Stop.HOLD, duty_limit=70)
+    claw_motor.run_angle(100, -80 , then=Stop.HOLD)
+    ######## the claw is open here
+    turning_motor.run_angle(speed=200,rotation_angle=-position, then=Stop.HOLD)
+    arm_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=10)
+    print(arm_motor.angle())
+    check_box()
+    ### behövs inte
+    #claw_motor.run_until_stalled(50, then=Stop.HOLD, duty_limit=70)
+    ##
+    wait(1000)
+    
+    return
+    
+     
+
+def place_the_box(position):
+    #arm_motor.run_angle(speed=100,rotation_angle=-215)
+    turning_motor.run_angle(speed=200,rotation_angle=position, then=Stop.HOLD)
+    #claw_motor.run_angle(80, -20 , then=Stop.HOLD)
+    arm_motor.run_until_stalled(200, then=Stop.COAST, duty_limit=10)
+    
+    #arm_motor.run_angle(speed=150,rotation_angle=-400)
+    
+    
+    wait(1150)
+    claw_motor.run_until_stalled(100, then=Stop.COAST, duty_limit=65)
+    #turning_motor.run_angle(speed=100,rotation_angle=-80, then=Stop.HOLD)
+    return
+
+def where_to_place(PO_RED,PO_BLUE,PO_YELLOW,PO_GREEN):
+    box_color=color_sensor.color()
+    
+    if box_color== Color.RED:
+        place_the_box(PO_RED)
+    elif box_color== Color.BLUE:
+        place_the_box(PO_BLUE)
+    elif box_color== Color.YELLOW:
+        place_the_box(PO_YELLOW)
+    elif box_color== Color.GREEN:
+        place_the_box(PO_GREEN)
+    # Oftast läsas gult som brun
+    elif box_color== Color.BROWN:
+        place_the_box(PO_YELLOW)
+    # oftast läsas blå som svart
+    elif box_color== Color.BLACK:
+        place_the_box(PO_YELLOW)
+    
+
+    
+    return
+
+def check_box():
+    claw_motor.reset_angle(0)
+    clawAngle= claw_motor.angle()
+    print(clawAngle)
+    wait(1000)
+   
+    claw_motor.run_until_stalled(-200, then=Stop.HOLD, duty_limit=70) # Close the claw. Check what is the angle at the close position is? check also if there are something what is the angle.
+    
+    print(claw_motor.angle())
+    if  -74 < claw_motor.angle() < 0 : # Check if the claw can close totaly.
+        ##claw_motor.run_target(100,0)
+        #arm_motor.run_angle(speed=100,rotation_angle=-175)
+        print(" OBJECT detected")
+
+    else  :
+        print("There is no box")
+        claw_motor.run_angle(100, 80 , then=Stop.HOLD)
+        arm_motor.run_angle(speed=200,rotation_angle=-400)
+        wait(10000)
+        arm_motor.run_angle(speed=200,rotation_angle=400)
+        check_box()
+
+
+
+    
+    return
+
+
+
+#allt detta kommer sen att gå in i en loop for att roboten ska fungera under hela tiden
+
+"""
+#userstory5
+reboot()
+
+#claw_motor.run_angle(100, -80 , then=Stop.HOLD)
+
+#take the box from this poistion
+takethebox(615)
+arm_motor.run_angle(speed=100,rotation_angle=-240)
+wait(2000)
+color_sensor.color()
+print(color_sensor.rgb())
+box_color=color_sensor.color()
+print(box_color)
+wait(8000)
+
+#Place the box function when the attrbiute is 600 it means that it is going in the opposite direction
+#place_the_box(300)
+
+# THE poistions of the colors is red for first cell and then blue, yellow and green
+where_to_place(450,450,300,150)
+"""
+
+
+#there are box at a location
+"""
+reboot()
+takethebox(615)
+check_box()
+
+"""
+
+def loop():
+    reboot()
+    #arm_motor.run_angle(speed=100,rotation_angle=-400)
+    pickup=615
+    count= 0
+    while True:
+       
+        takethebox(pickup)
+
+        
+        
+        arm_motor.run_angle(speed=100,rotation_angle=-240)
+        wait(1000)
+        color_sensor.color()
+        print(color_sensor.rgb())
+        box_color=color_sensor.color()
+        print(box_color)
+        wait(1000)
+        PO_RED= 450
+        PO_BLUE= 450
+        PO_YELLOW= 300
+        PO_GREEN= 150
+        turning_motor.reset_angle(0)
+        
+        
+        where_to_place(PO_RED,PO_BLUE,PO_YELLOW,PO_GREEN)
+       
+        grader =  turning_motor.angle()
+        print(grader)
+        pickup= (grader)
+        
+        
+    return
+
+    
+
+loop()
+#check_box()
+
+
+
+
+
+
+
